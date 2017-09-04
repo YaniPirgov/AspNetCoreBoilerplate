9b2a33b240e9c0c9126b67e135ad517f222d0f2a
oposumi <irakli.gabisonia@wandio.com>
2017-09-04 22:44:51 +0400

    udated .net framework

--- Boilerplate.Api/Program.cs
+++ Boilerplate.Api/Program.cs
@@ -1,6 +1,12 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace Boilerplate.Api
 {
@@ -8,15 +14,12 @@ namespace Boilerplate.Api
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
